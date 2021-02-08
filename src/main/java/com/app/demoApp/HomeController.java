7a8a57ce72f79d045eb41a05afec969664160b59
GK Techy <glapskrish@gmail.com>
2021-02-08 16:09:37 +0530

    Commit to Server

---
+++ src/main/java/com/app/demoApp/HomeController.java
@@ -0,0 +1,13 @@
+package com.app.demoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String HomeController() {
+		return "Welcome To Spring Boot App";
+	}
+}
---
+++ src/test/java/com/app/demoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.demoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
